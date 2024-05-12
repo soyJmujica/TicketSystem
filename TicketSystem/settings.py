2b43319bfb404ba91140e10e32b29b8052ccbdb4
Jose Mujica <master1495@gmail.com>
2024-05-12 15:51:07 -0400

    base de datos sqlite3 agregada, para hacer el dummy

--- TicketSystem/settings.py
+++ TicketSystem/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fm1)xegmto6*4!^u%w#-93oi%m*#$)ryl$p7jz#edpwp-3w1z_"
+SECRET_KEY = "SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
